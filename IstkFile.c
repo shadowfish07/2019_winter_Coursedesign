e3929751e964fd95c1954a3852f0212b41e74994
Mxy-11 <2937676283@qq.com>
2019-12-14 21:56:14 +0800

    m

---
+++ IstkFile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int IstkFile(const char*s)
+{
+	char*pFile;
+	pFile=strrchr(s,'.');
+	printf("%s\n",pFile);
+		if(strcmp(pFile,".tk")==0)
+			return 1;
+		else
+			return 0;
+}
+
