dba1759628f6e50d6e95fe17b4e75227025739f9
Mxy-11 <2937676283@qq.com>
2019-12-18 14:54:45 +0800

    Create AddTkFile.c

---
+++ AddTkFile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void AddTkFile(char*name)
+{
+	FILE*fp;
+	char fileName[100]={0};
+	strcpy(fileName,"C:\\");
+	strcat(fileName,name);
+	strcat(fileName,".tk");
+	fp=fopen(fileName,"w+");
+	if(!fp)
+	{
+		printf("can't open file %s\n",fileName);
+		exit(1);
+	}
+	fclose(fp);
+}
