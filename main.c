988856e2d9373ebd819a0bb71ffe660199951e57
Mxy-11 <2937676283@qq.com>
2020-01-02 15:55:25 +0800

    Update main.c

--- main.c
+++ main.c
@@ -66,7 +66,7 @@ void AddQuestionSystem()
 
 int main()
 {
-    printf("欢迎使用 单项选择题标准化考试系统\n");
+	printf("欢迎使用 单项选择题标准化考试系统\n");
     printf("正在扫描题库...\n");
     databases= *InitDatabaseList(10);
     ReadPathTkFiles();
